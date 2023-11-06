3d69a37375b7e56fa5c4b5846e99d5f93e80c31c
Anmol Sandhu <anmolrattansandhu@gmail.com>
2023-11-06 00:47:33 -0500

    added full corr + convolution2d

--- correlate.py
+++ correlate.py
@@ -1,14 +1,23 @@
 import numpy as np
 
-def correlate2d(input_array, kernel):
+def correlate2d(input_array, kernel, mode='valid'):
     # Get dimensions of input array and kernel
     input_rows, input_cols = input_array.shape
     kernel_rows, kernel_cols = kernel.shape
     
     # Determine the dimensions of the output array based on the mode
-    output_rows = input_rows - kernel_rows + 1
-    output_cols = input_cols - kernel_cols + 1
-
+    if mode == 'full':
+        output_rows = input_rows + kernel_rows - 1
+        output_cols = input_cols + kernel_cols - 1
+        # Pad the input array with zeros
+        padded_input = np.pad(input_array, ((kernel_rows - 1, kernel_rows - 1), (kernel_cols - 1, kernel_cols - 1)), mode='constant')
+    elif mode == 'valid':
+        output_rows = input_rows - kernel_rows + 1
+        output_cols = input_cols - kernel_cols + 1
+        padded_input = input_array
+    else:
+        raise ValueError(f"Unsupported mode: {mode}")
+    
     # Initialize the output array
     output_array = np.zeros((output_rows, output_cols))
     
@@ -16,13 +25,14 @@ def correlate2d(input_array, kernel):
     for i in range(output_rows):
         for j in range(output_cols):
             # Extract the region of the input array that overlaps with the kernel at this position
-            region = input_array[i:i + kernel_rows, j:j + kernel_cols]
+            region = padded_input[i:i + kernel_rows, j:j + kernel_cols]
             # Compute the sum of element-wise products and store the result in the output array
             output_array[i, j] = np.sum(region * kernel)
     
     return output_array
 
-input_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-kernel = np.array([[1, 0], [0, -1]])
-output_array = correlate2d(input_array, kernel)
-print(output_array)
+def convolution2d(input_array, kernel):
+    # Flip the kernel
+    kernel = np.flip(kernel)
+    # Call the correlate2d function with the flipped kernel
+    return correlate2d(input_array, kernel)
